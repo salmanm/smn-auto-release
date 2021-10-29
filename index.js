ab638f0c28da2bcf34292089245a61d5ef89bada
Salman <salmanmitha@gmail.com>
2021-10-29 17:40:18 +0530

    fix: a bug was fixed

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ module.exports = () => {
   console.log('test')
   console.log('a new feature')
   console.log('another new feature')
-  console.log('a bug fixed')
+
   console.log('added feature A')
   console.log('added feature B')
 }
